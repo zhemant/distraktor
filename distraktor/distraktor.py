006380ac3fea9582a2315e0641111f65698befe4
Hemant Zope <hemant.zope@fokus.fraunhofer.de>
2021-05-14 00:25:03 +0200

    Modify functionality of distraktor * use apscheduler * support duration

--- distraktor/distraktor.py
+++ distraktor/distraktor.py
@@ -1,26 +1,43 @@
 import yaml
 import random
-import threading
-import time
+import os
+import asyncio
 import distraktor.notification as notify
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from durations import Duration
 
 
-def reminder(messages):
-    interval_water = 5
-    while True:
-        try:
-            msg = random.choice(messages)
-            notify.notification_send(title="drink water", message=msg)
-            time.sleep(1)
-        except KeyboardInterrupt:
-            break
+def reminder(title="", messages="", icon=""):
+    try:
+        msg = random.choice(messages)
+        icon = random.choice(icon)
+        notify.notification_send(title=title, message=msg, icon=icon)
+    except KeyboardInterrupt:
+        pass
 
 
 def main():
-    config = yaml.load(open("data/data.yaml"), Loader=yaml.BaseLoader)
-    water_messages = config["water_messages"]
-    print(water_messages)
-    reminder(water_messages)
+    data = yaml.load(open("data/data.yaml"), Loader=yaml.BaseLoader)
+
+    scheduler = AsyncIOScheduler()
+
+    for item in data["distraktor"]:
+        interval = Duration(item["interval"]).to_seconds()
+        scheduler.add_job(
+            reminder,
+            trigger="interval",
+            args=[item["name"], item["messages"], item["icons"]],
+            seconds=interval,
+        )
+
+    scheduler.start()
+
+    print("Press Ctrl+{0} to exit".format("Break" if os.name == "nt" else "C"))
+    try:
+        asyncio.get_event_loop().run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+        pass
 
 
 if __name__ == "__main__":
