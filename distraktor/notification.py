5282700c0aa69a7670e20a9ce17fc4ce7abb9e18
Hemant Zope <hemant.zope@fokus.fraunhofer.de>
2021-05-14 00:18:23 +0200

    add icon support in notification

--- distraktor/notification.py
+++ distraktor/notification.py
@@ -1,12 +1,12 @@
 from notifypy import Notify, exceptions
 
 
-def notification_send(title="Empty", message="No message"):
+def notification_send(title="Empty", message="No message", icon=""):
     notify = Notify()
     notify.title = title
     notify.message = message
     try:
-        notify.icon = "data/drinking_water.png"
+        notify.icon = icon
         notify.send(block=False)
     except (exceptions.InvalidIconPath):
         pass
